dbe9128c28751cfe8e43d98f96ad5a3d3c3f301b
David Akehurst <david.akehurst@itemis.de>
2019-12-03 14:43:06 +0100

    kt2ts version bump

--- net.akehurst.kotlin.json/json/build.gradle.kts
+++ net.akehurst.kotlin.json/json/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("net.akehurst.kotlin.kt2ts") version "1.0.0"
+    id("net.akehurst.kotlin.kt2ts") version "1.2.0"
 }
 
 val version_kotlinx:String by project
@@ -9,23 +9,10 @@ dependencies {
 }
 
 
-val tsdDir ="${buildDir}/tmp/jsJar/ts"
-
-kotlin {
-    sourceSets {
-        val jsMain by getting {
-            resources.srcDir("${tsdDir}")
-        }
-    }
-}
-
 kt2ts {
-    localJvmName.set("jvm8")
-    modulesConfigurationName.set("jvm8RuntimeClasspath")
-    outputDirectory.set(file("${tsdDir}"))
+    jvmTargetName.set("jvm8")
+
     classPatterns.set(listOf(
             "net.akehurst.kotlin.json.*"
     ))
 }
-tasks.getByName("generateTypescriptDefinitionFile").dependsOn("jvm8MainClasses")
-tasks.getByName("jsJar").dependsOn("generateTypescriptDefinitionFile")
